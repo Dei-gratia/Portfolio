687f2d300a08a2b498e7a28cc0e00dd410f52fd6
Dei-gratia <ndmadyavanhu@gmail.com>
2022-06-23 20:43:44 +0530

    changed settings

--- nenyasha/settings.py
+++ nenyasha/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = TRUE
+DEBUG = False
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost',
                  '127.0.0.1', 'nenyasha.pythonanywhere.com']
