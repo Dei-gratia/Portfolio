266785b0e1c0b3821f2653d8e101186a2e8ec677
Dei-gratia <ndmadyavanhu@gmail.com>
2022-06-23 19:16:37 +0530

    changed settings

--- nenyasha/settings.py
+++ nenyasha/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1p-x+y@^55d4n^)+mmyg2sdouqs=5^m=^nw6f=g(o0%#m^gc44'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
